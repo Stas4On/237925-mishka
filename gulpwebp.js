a67e165f57e236eee32c22b21fe99e22e5d79c47
Stanislav Kuznetsov <stasmgpu@yandex.ru>
2017-10-16 23:05:19 +0400

    Добавляет svg в футер и в блок О нас + обводки для кастомных чекбоксов и радио

--- gulpwebp.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var webp = require("gulp-webp");
-
-gulp.task("webp", function () {
-  return gulp.src("img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("img"));
-});
